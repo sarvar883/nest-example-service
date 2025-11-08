af4a34a265b2d36da21cf0c03b2606490412cbfa
Sarvar Sharapov <sarvarik883@gmail.com>
2025-11-08 13:21:36 +0500

    minor bug fix

--- src/app/services/worker/app.module.ts
+++ src/app/services/worker/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from 'di/modules/infrastructure/config-module';
 import { MongoDBModule } from 'di/modules/infrastructure/mongodb-module';
+import { TaskProcessorModule } from 'di/modules/infrastructure/task-processor-module';
 import { TaskModule } from 'di/modules/infrastructure/task-module';
 
 @Module({
-    imports: [ConfigModule, MongoDBModule, TaskModule],
+    imports: [ConfigModule, MongoDBModule, TaskProcessorModule, TaskModule],
 })
 export class AppModule {}
